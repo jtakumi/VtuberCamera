e007e7b393145726d92e00a7669b6c68bd831dca
STORM-PC\Takum <takumi87jn@gmail.com>
2023-10-08 16:25:25 +0900

    opening screen onClick function

--- app/src/main/java/com/example/vtubercamera/OpeningActivity.kt
+++ app/src/main/java/com/example/vtubercamera/OpeningActivity.kt
@@ -4,26 +4,30 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.vtubercamera.databinding.ActivityOpeningBinding
 
-class OpeningActivity : AppCompatActivity() {
+class OpeningActivity : AppCompatActivity(),View.OnClickListener {
     private lateinit var binding: ActivityOpeningBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOpeningBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.enterTap.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            playSound()
-            finish()
-        }
+        binding.enterTap.setOnClickListener(this)
     }
-    private fun playSound(){
-       val mediaPlayer = MediaPlayer.create(this,R.raw.enter_app).apply {
-           isLooping = false
-           start()
-       }
+
+    override fun onClick(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        playSound()
+        finish()
+    }
+
+    private fun playSound() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.enter_app).apply {
+            isLooping = false
+            start()
+        }
     }
 }
