72e7f838e468c58d68a16842d5793e396635d4d1
STORM-PC\Takum <takumi87jn@gmail.com>
2023-10-15 11:00:57 +0900

    最終行追加

--- app/src/main/java/com/example/vtubercamera/extentions/extentionFunction.kt
+++ app/src/main/java/com/example/vtubercamera/extentions/extentionFunction.kt
@@ -1,8 +1,6 @@
 package com.example.vtubercamera.extentions
 
 import android.media.MediaPlayer
-import com.example.vtubercamera.R
-
 
 fun playSound(mediaPlayer: MediaPlayer) {
     mediaPlayer.apply {
