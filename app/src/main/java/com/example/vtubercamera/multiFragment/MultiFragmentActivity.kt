205a9a06de554db4c3abfcd63f1f18ded4a0678e
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2023-10-28 18:28:04 +0900

    MultiFragmentActivity onOptionSelected added

--- app/src/main/java/com/example/vtubercamera/multiFragment/MultiFragmentActivity.kt
+++ app/src/main/java/com/example/vtubercamera/multiFragment/MultiFragmentActivity.kt
@@ -1,8 +1,8 @@
 package com.example.vtubercamera.multiFragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.vtubercamera.R
 import com.example.vtubercamera.databinding.ActivityMultiFragmentBinding
 
@@ -13,5 +13,22 @@ class multiFragmentActivity : AppCompatActivity() {
         binding= ActivityMultiFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val toolbar =binding.multiFragmentToolbar
+        setSupportActionBar(toolbar)
+        val fragmentManager =supportFragmentManager
+        val initFragment = recyclerItemFragment()
+        fragmentManager.beginTransaction().replace(R.id.multi_fragment_container,initFragment).commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            androidx.appcompat.R.id.home->{
+                val fragmentManager =supportFragmentManager
+                val goBackToFragment = recyclerItemFragment()
+                fragmentManager.beginTransaction().replace(R.id.multi_fragment_container,goBackToFragment).commit()
+                goBackToFragment.refreshAuto()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
