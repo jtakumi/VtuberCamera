ecab801853d4430c48eabedc1f8f339e0cdca538
jtakumi <takumi87jn@gmail.com>
2025-08-23 23:00:36 +0900

    create permission preview

--- app/src/main/java/com/example/vtubercamera/ui/components/PermissionRequestComponent.kt
+++ app/src/main/java/com/example/vtubercamera/ui/components/PermissionRequestComponent.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.vtubercamera.R
 
@@ -31,8 +34,30 @@ fun PermissionRequestComponent(
     ) {
         Text(title)
         Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = onButtonClick) {
+        Button(onClick = onButtonClick, colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)) {
             Text(buttonText)
         }
     }
+}
+
+@Preview(locale = "ja")
+@Preview(locale = "en")
+@Composable
+fun CameraPermissionRequestComponentPreview() {
+    PermissionRequestComponent(
+        title = stringResource(id = R.string.camera_permission_required),
+        buttonText = stringResource(id = R.string.grant_camera_permission),
+        onButtonClick = {}
+    )
+}
+
+@Preview(locale = "ja")
+@Preview(locale = "en")
+@Composable
+fun PhotoPermissionRequestComponentPreview() {
+    PermissionRequestComponent(
+        title = stringResource(id = R.string.grant_storage_permission),
+        buttonText = stringResource(id = R.string.grant_storage_permission),
+        onButtonClick = {}
+    )
 }
