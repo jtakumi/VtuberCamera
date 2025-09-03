2c2567dbc3d4409d2e15cc8b18d672247fceb6f9
jtakumi <takumi87jn@gmail.com>
2025-09-04 00:18:21 +0900

    setting ui layer

--- app/src/main/java/com/example/vtubercamera/ui/components/PhotoDetailComponent.kt
+++ app/src/main/java/com/example/vtubercamera/ui/components/PhotoDetailComponent.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.vtubercamera.R
-import com.example.vtubercamera.ui.viewmodels.PhotoItem
+import com.example.vtubercamera.data.PhotoItem
 
 @Composable
 fun PhotoDetailView(
